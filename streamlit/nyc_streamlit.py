33a78e1a3a84f57465982b11b8bb9c8151e7ea9d
Jordan Samek <jordan.samek@gmail.com>
2021-02-10 14:45:23 -0500

    streamlit tutorial finished

--- streamlit/nyc_streamlit.py
+++ streamlit/nyc_streamlit.py
@@ -61,3 +61,67 @@ st.json({top_host_1.iloc[0].host_name: top_host_1\
             [["name", "neighbourhood", "room_type", "minimum_nights", "price"]]\
                 .sample(2, random_state=4).to_dict(orient="records")})
 
+st.header("What is the distribution of property proce?")
+st.write("""Select a custom price range from the side bar to update the histogram below displayed as s Plotly chart using
+[`st.plotly_chart`](https://streamlit.io/docs/api.html#streamlit.plotly_chart)""")
+values = st.sidebar.slider("Price range", float(df.price.min()), float(df.price.clip(upper=1000.).max()), (50., 300.))
+f = px.histogram(df.query(f"price.between{values}"), x="price", nbins=15, title="Price Distribution")
+f.update_xaxes(title="Price")
+f.update_yaxes(title="No. of listings")
+st.plotly_chart(f)
+
+st.header("What is the distribution of availability in various neighbourhoods?")
+st.write("Using a radio button restricts selection to only one option at a time")
+st.write("Notice how we use a static table below instead of a dataframe. \
+    Unlike a dataframe, if content overflows out of the section margin, \
+        a static table does not automatically hide it inside a scrollable area. \
+            Instead, the overflowing content remains visible.")
+neighbourhood = st.radio("Neighbourhood", df.neighbourhood_group.unique())
+show_exp = st.checkbox("Include expensive listings")
+show_exp = " and price<200" if not show_exp else ""
+
+@st.cache
+def get_availability(show_exp, neighbourhood):
+    return df.query(f"""neighbourhood_group==@neighbourhood{show_exp}\
+        and availability_365>0""").availability_365.describe(\
+            percentiles=[.1, .25, .5, .75, .9, .99]).to_frame().T
+
+st.table(get_availability(show_exp, neighbourhood))
+st.write("At 169 days, Brooklyn has the lowest average availability. At 226, Staten Island has the highest average availability.\
+    If we include expensive listings (price>=$200), the numbers are 171 and 230 respectively.")
+st.markdown("_**Note:** There are 18431 records with `availability_365` 0 (zero), which we've ignores._")
+
+df.query("availability_365>0").groupby("neighbourhood_group")\
+    .availability_365.mean().plot.bar(rot=0).set(title="Average availability by neighbourhood group",
+    xlabel="Neighbourhood group", ylabel="Avg. Availability (in no. of days)")
+st.pyplot()
+
+st.header("Properties by number of reviews")
+st.write("Enter a range of numbers in the sidebar to view the properties whose review count falls in that range.")
+minimum = st.sidebar.number_input("Minimum", min_value=0)
+maximum = st.sidebar.number_input("Maximum", min_value=0, value=5)
+if minimum > maximum:
+    st.error("Please enter a valid range")
+else:
+    df.query("@minimum<=number_of_reviews<=@maximum").sort_values("number_of_reviews", ascending=False)\
+        .head(50)[['name', 'number_of_reviews', 'neighbourhood', 'host_name', 'room_type', 'price']]
+
+st.write("486 is the highgest number of reviews and two properties have it. Both are in the East Elmhurst \
+    neighbourhood and are private rooms with prices $65 and $45. \
+        In general, listings with >400 reviews are priced below $100. \
+            A few are between $100 and $200, and only one is priced above $200.")
+
+st.header("Images")
+pics = {
+    "Cat": "https://cdn.pixabay.com/photo/2016/09/24/22/20/cat-1692702_960_720.jpg",
+    "Puppy": "https://cdn.pixabay.com/photo/2019/03/15/19/19/puppy-4057786_960_720.jpg",
+    "Sci-fi city": "https://storage.needpix.com/rsynced_images/science-fiction-2971848_1280.jpg"
+}
+pic = st.selectbox("Picture choices", list(pics.keys()), 0)
+st.image(pics[pic], use_column_width=True, caption=pics[pic])
+
+st.markdown("## Party Time!")
+st.write("Click below to celebrate getting through the tutorial")
+btn = st.button("Celebrate!")
+if btn:
+    st.balloons()
